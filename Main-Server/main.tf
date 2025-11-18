bef5dc2a130293a5e30a19328944059063ec839c
KjTejasvi <kjallamesh387@gmail.com>
2025-11-18 21:24:16 +0530

    added jenkinsh script

--- Main-Server/main.tf
+++ Main-Server/main.tf
@@ -1,1 +1,18 @@
-provider
+provider "aws" {
+    region = "ap-south-1"
+    
+}
+
+resource "aws_instance" "Jenkins_server" {
+    ami           = "ami-02b8269d5e85954ef"
+    instance_type = "t3.micro"
+    key_name = ["Docker-key"]
+    associate_public_ip_address = true
+   
+
+    tags = {
+        Name = "ExampleInstance"
+    }
+}
+
+user_data = 
